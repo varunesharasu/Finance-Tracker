dc676bc30f35be7a59c97def2fd15553a8d37243
VaruneshT <tvarunesharasu@gmail.com>
2026-05-23 19:16:15 +0530

    Updated api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api' || 'https://finance-tracker-6li5.onrender.com';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
